1ffad2415701602d7a0f09d85e3547e401005dcb
Lijo Hernan <lijo.hernan@gmail.com>
2024-02-25 04:05:29 -0300

    imagen al home

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import './header.css'
+import classes from './header.module.css'
 import logo from "../../../public/data/EscudoPiroSinFondo.png"
 import { Link } from 'react-router-dom';
 
 const header = () => {
     return (
-        <header className='header'>
-            <Link to='/' className="header__cont"><img src= {logo} alt="Pirovano" className='header__img'/></Link>
-            <div className='header__title'>
-                <h1 className="header__h1">Servicio Tomografia Computada</h1>
-                <h2 className='header__h2'>Pases de Guardia</h2>            
+        <header className={classes.header}>
+            <Link to='/' className={classes.header__link}><img src= {logo} alt="Pirovano" className={classes.header__img}/></Link>
+            <div className={classes.header__title}>
+                <h1 className={classes.header__h1}>Servicio Tomografia Computada</h1>
+                <h2 className={classes.header__h2}>Pases de Guardia</h2>            
             </div>
         </header>
     );
