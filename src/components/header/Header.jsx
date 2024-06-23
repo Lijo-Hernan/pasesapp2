13da8cfac80b14a3fa556501670e674ad0251768
Hernán Pablo Lijó <lijo.hernanpablo@gmail.com>
2024-06-22 23:26:56 -0300

    titulos login

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,8 +9,8 @@ const header = () => {
             <Link to='/' className={classes.header__link}><img src='https://firebasestorage.googleapis.com/v0/b/pasesapp-d01af.appspot.com/o/EscudoPiroSinFondo.png?alt=media&token=2486eaa3-05a0-414e-a419-524473e218ce' 
             alt="Pirovano" className={classes.header__img}/></Link>
             <div className={classes.header__title}>
-                <h1 className={classes.header__h1}>Servicio Tomografia Computada</h1>
-                <h2 className={classes.header__h2}>Pases de Guardia</h2>
+                <h2 className={classes.header__h1}>Servicio Tomografia Computada</h2>
+                <h3 className={classes.header__h2}>Pases de Guardia</h3>
             </div>
             <Link to='/historial' className={classes.header__boton}><b>Historial de Reportes</b></Link>            
         </header>
